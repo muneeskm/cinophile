df3e0a43b6f68c3d02de190725895ccb0bdd6d70
Munees K M <muneesrafi@gmail.com>
2026-08-30 17:05:44 +0530

    Configure Vercel serverless deployment

--- Cinophile/wsgi.py
+++ Cinophile/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for Cinophile project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cinophile.settings')
 
 application = get_wsgi_application()
+
+# Required for Vercel
+app = application
